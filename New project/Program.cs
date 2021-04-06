4f6631d282ffbf03bd7a761c215b5e38f9bd33ef
Sofiane taleb <str14.don@gmail.com>
2021-04-06 21:56:39 +0200

    Add comments to 'Program.cs'

--- New project/Program.cs
+++ New project/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.IO;
+using System.Linq;
+using System.Text;
 
 namespace New_project
 {
@@ -6,7 +9,39 @@ namespace New_project
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //1 read numbers from file
+            //2 write numbers to file2 
+            //3 print numbers In reverse number
+            // 20,25,0,12,3,5,1
+            //4 print max and min number
+            // max:25 , min:0
+            //5 print numbers in order
+            // 0,1,3,5,12,20,25
+             Read();
+            Write();
+            Console.ReadLine();
         }
+        const string Filename = "File1.txt";
+        //When i run he tell me file not found
+       static void Read()
+       {
+           StreamReader sr = new StreamReader(Filename);
+           string data = sr.ReadLine();
+
+            
+            Console.WriteLine(data);
+            sr.Close();
+        }
+        // when i run this he don't put the number in File2
+        const string filename = "File2.txt";
+        static void Write()
+        {
+            StreamWriter sw = new StreamWriter(filename);
+            string s = "1 3 5 12 0 25 20  ";
+            Console.WriteLine(s);
+            sw.Close();
+        }
+      
+
     }
 }
