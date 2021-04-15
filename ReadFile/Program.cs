21c0f1f7f8ceeb5e7f40283850bb114de2cc8a52
Sofiane taleb <str14.don@gmail.com>
2021-04-15 18:48:23 +0200

    Remove "//" from file

--- ReadFile/Program.cs
+++ ReadFile/Program.cs
@@ -8,7 +8,7 @@ namespace ReadFile
     {
         static void Main(string[] args)
         {
-            // when i run the code, the comments that start with "//" in the result i get 0 in her place
+            
             string fileContents = File.ReadAllText(@"File1_temporary.txt");
             string line = '\r'.ToString() + '\n'.ToString();
             fileContents = fileContents.Replace(line, '\n'.ToString());
@@ -19,17 +19,9 @@ namespace ReadFile
                 int.TryParse(array[i], out intArray[i]);
             }
             foreach (int i in intArray)
-                Console.WriteLine(i);
+            Console.WriteLine(i);
 
-           if (line.StartsWith("//"))
-           {
-                Console.WriteLine(line);
-                fileContents += line;
-           }
            Console.Read();
-
-           
-
 
         }
     }
