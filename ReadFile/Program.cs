8e9245cadf77558af78eb0d677a52a766368c0cf
mfaci2000 <mfaci2000@yahoo.com>
2021-04-15 12:03:02 -0500

    Added  note 1.

--- ReadFile/Program.cs
+++ ReadFile/Program.cs
@@ -8,7 +8,14 @@ namespace ReadFile
     {
         static void Main(string[] args)
         {
-            
+            // Note 1: Change your code to use ReadLine, similar to this link.
+            // In you case, you read line by line, then:
+            // 1. Write the line you read to the screen.
+            // 2. Add the line to the array.
+            // push back when done.
+            // 
+            // https://docs.microsoft.com/en-us/dotnet/csharp/programming-guide/file-system/how-to-read-a-text-file-one-line-at-a-time
+            // 
             string fileContents = File.ReadAllText(@"File1_temporary.txt");
             string line = '\r'.ToString() + '\n'.ToString();
             fileContents = fileContents.Replace(line, '\n'.ToString());
