34dbc79e10d13dba5ff776799d2422cf594ea857
mfaci2000 <mfaci2000@yahoo.com>
2021-04-15 21:23:26 -0500

    See Note 1

--- ReadFile/Program.cs
+++ ReadFile/Program.cs
@@ -20,7 +20,7 @@ namespace ReadFile
             // 
            
             int counter = 0;
-            string line;
+            //string line;
             
 
 
@@ -29,22 +29,28 @@ namespace ReadFile
 
             
             int[] inputValues = new int[7];
-            
 
-            while ((line = file.ReadLine()) != null)
+            // I am replacing this line with other lines to make it easier to see
+            // the execution logic.
+            // while ((line = file.ReadLine()) != null)
+            var number = file.ReadLine(); // read the first line in the file
+            while (number != null) 
             {
-                Console.WriteLine(line);
-                inputValues[counter] = int.Parse(line);
-                counter++;
-                
+                //Console.WriteLine(line);
+                inputValues[counter] = int.Parse(number);
+                counter++; 
+                number = file.ReadLine(); // read the next line in the file
             }
 
             file.Close();
-            Console.WriteLine($"File has {counter} lines" , inputValues);
-            Console.ReadLine();
+            // We do not need the next 2 lines.
+            //Console.WriteLine($"File has {counter} lines" , inputValues);
+            //Console.ReadLine();
 
             
-
+            // At this point. we have an array that holds 7 numbers.
+            // Note 1: Write code that writes the numbers in the array to the screen
+            // push back wghen done!
 
         }
     }
