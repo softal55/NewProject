a14f89fba9c37aa6fa3b2595831e9dae17d70cae
Sofiane taleb <str14.don@gmail.com>
2021-04-18 17:18:16 +0200

    See the line 58 and 59

--- ReadFile/Program.cs
+++ ReadFile/Program.cs
@@ -33,28 +33,36 @@ namespace ReadFile
             // I am replacing this line with other lines to make it easier to see
             // the execution logic.
             // while ((line = file.ReadLine()) != null)
-            var number = file.ReadLine(); // read the first line in the file
+            var number = file.ReadLine();// read the first line in the file
+          
             while (number != null) 
             {
                 
                 inputValues[counter] = int.Parse(number);
-                Console.WriteLine(number);
+                
                 counter++; 
                 number = file.ReadLine(); // read the next line in the file
             }
 
-            file.Close();
+            // file.Close();
             // We do not need the next 2 lines.
             //Console.WriteLine($"File has {counter} lines" , inputValues);
             //Console.ReadLine();
 
-            
 
-            
+
+
             // At this point. we have an array that holds 7 numbers.
             // Note 1: Write code that writes the numbers in the array to the screen
             // push back wghen done!
-
+           //When i use "F11" the compiler see just the frist while loop
+           //Why?
+            while ((number = Console.ReadLine()) != null)
+            {
+                if (number != null)
+               Console.WriteLine(number);
+            }     
+            file.Close();
         }
     }
 }
