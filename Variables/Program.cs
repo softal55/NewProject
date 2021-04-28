0dbbee686161b36acc0c39921db298d00a370dfa
mfaci2000 <mfaci2000@yahoo.com>
2021-04-28 10:03:51 -0500

    Add project variables

---
+++ Variables/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Variables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x;
+
+            x = 3;
+
+            int y;
+            
+            y = x;
+
+            x = 5;
+
+            Console.WriteLine($"x = {x}");
+            Console.WriteLine($"y = {y}");
+        }
+    }
+}
