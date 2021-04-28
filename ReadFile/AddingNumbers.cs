566c43db2f9070a70468e3cbc39e6505b8dff60e
Sofiane taleb <str14.don@gmail.com>
2021-04-28 13:20:39 +0200

    Finish the task 1 , 2 and 3 See the line 49

--- ReadFile/AddingNumbers.cs
+++ ReadFile/AddingNumbers.cs
@@ -11,9 +11,10 @@ namespace ReadingFile
         // I wrrote this ode  :
         public static int  Add(int [] myData)
         {
+            // int [] myData
             _ = File.ReadAllLines(@"File1_temporary.txt");
-            //int sum = 0;
-            int sum = myData.Sum();
+            
+           int sum = myData.Sum();
             return sum;
             //int sumOfNumber = myData.Count();
             //for (int i = 0; i < sumOfNumber; i++)
@@ -24,6 +25,31 @@ namespace ReadingFile
             //return myData;
         }
 
-        
+        public static int AddOne(int x)
+        {
+           x+= 0;
+            return x+1;
+        }
+        public static int Add2Numbers(int a, int b)
+        {
+            _ = a + b;
+            return a + b;
+        }
+        public static int[] ConvertToArray(int y, int z)
+        {
+            // declare an array of integer
+            int[] smallArray = new int[2];
+            int numberOfArray = smallArray.Count();
+            Console.WriteLine("Task 3:");
+            for (int i = 0; i < numberOfArray; i++)
+            {
+                Console.WriteLine(y);
+                Console.WriteLine(z);
+
+            }
+            
+            //return an array of integers
+            return smallArray;
+        }
     }
 }
--- ReadFile/Program.cs
+++ ReadFile/Program.cs
@@ -21,29 +21,37 @@ namespace ReadFile
             // Add a class called "AddingNumbers" to the project. The class has one method called "Add"
             // The "Add" method takes the array, which is "myData", adds all the numbers in the array and returns the sum.
             int sumOfNumbers = AddingNumbers.Add(myData);
-
+            Console.WriteLine("-------------------------------------------------");
             Console.WriteLine ("Sum = {0}", sumOfNumbers);
             //Console.ReadKey();
 
             // Task 1:
             int x = 6;
             // This method return x+ 1 
+            Console.WriteLine("-------------------------------------------------");
+            Console.WriteLine("Task 1:");
             int addOne = AddingNumbers.AddOne(x);
-            Console.WriteLine("X = {0}", x); // 7
+            Console.WriteLine("X = {0}", addOne); // 7
 
             // Task 2
-            int a = 6;
-            int b = 7;
+           int a = 6;
+           int b = 7;
             // This method return the sum of a+b
-            int add2Numbers = AddingNumbers.Add2Numbers(a,b);
-            Console.WriteLine("X = {0}", x); //  13
+            Console.WriteLine("-------------------------------------------------");
+            Console.WriteLine("Task 2:");
+            int add2Numbers = AddingNumbers.Add2Numbers( a, b);
+            Console.WriteLine("X = {0}", add2Numbers); //  13
 
             // Task 3
             int y = 5;
             int z = 22;
             // this method return an array of integers which are y and z.
+            // the compiler print 5 and 22 twice
+            Console.WriteLine("-------------------------------------------------");
             int[] smallArray = AddingNumbers.ConvertToArray(y, z);
+  
 
+            
             // Use a while loop to display the numbers in the array smallArray
             // It should print 5 and 22
 
