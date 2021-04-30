f46316b9bb3cfdcf8be73632dd6e6bc25e2ccf59
mfaci2000 <mfaci2000@yahoo.com>
2021-04-30 17:24:06 -0500

    Project colors

---
+++ Colors/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Colors
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Task: using the switch statement
+            // Watch this video:
+            // https://www.youtube.com/watch?v=soKhwNYxs2I
+            // 1. Read a number from the keyboard
+            // 2. depending on the number entered, disply the following:
+            //    Your favorite color is : COLOR
+            //    for example, if the user enters 1, you diplay:
+            //     Your favorite color is : RED
+            // valid inputs are
+            // 1 for RED
+            // 2 for BLUE
+            // 3 for GREEN
+            // 4 for YELLOW
+            // 5 for BROWN
+            // 6 for WHITE
+            // 7 for BLACK
+            // if the user enters another value, diplay 
+            // Your entered an invalid number, please try again.
+            // 
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
