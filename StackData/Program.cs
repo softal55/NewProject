dc421ebb05be65b4c907f57a6979aadf73b445b7
mfaci2000 <mfaci2000@yahoo.com>
2021-05-10 22:44:28 -0500

    Stack in C#

---
+++ StackData/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace StackData
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // here is your task
+            // 1. read the "StackDataFile" and print it in reverse order
+            // Your output will be lile this:
+            //line 6
+            //line 5
+            //line 4
+            //line 3
+            //line 2
+            //line 1
+
+            // Watch this video for a hint.
+            // https://www.youtube.com/watch?v=tW75yz3X_M4
+            // Let me know if you need clarifications.
+        }
+    }
+}
