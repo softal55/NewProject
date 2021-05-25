8bbec7a556736de50e1d1cbad95fe64b9bf63013
mfaci2000 <mfaci2000@yahoo.com>
2021-05-25 11:48:34 -0500

    Dictionary

---
+++ DictionaryLesson/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+// Watch these videos on dictiony in C#
+// https://www.youtube.com/watch?v=jOLFsT6j9Wg
+// 
+namespace DictionaryLesson
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Task 1:
+            // Create a class called "Translation" in a file called "Translation.cs"
+            // The class has 2 properties
+            // 1. public string Word;
+            // 2. public string Meaning;
+
+            // Task 2:
+            // Add a method to the Translation class.
+            // The method takes a file name and return a list of object of type 'Translation'
+            // Each object contains the word and the meaning from the file.
+            // List<Translation> wordsInFile = Translation.GetDictionaryData(filename);
+
+            // Diplya the number of objects in the list
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
