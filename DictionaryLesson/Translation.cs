60336710eb096f6a393b46865e5a7a4ea93f5abe
mfaci2000 <mfaci2000@yahoo.com>
2021-05-26 12:11:13 -0500

    Hints

--- DictionaryLesson/Translation.cs
+++ DictionaryLesson/Translation.cs
@@ -14,7 +14,10 @@ namespace DictionaryLesson
         
         public static List<Translation> GetDictionaryData(string filename)
         {
-            
+            // Hint 1
+            // define  a dictinary  called 'wordsInDictionary' of type <string, string>
+            Dictionary<string, string> wordsInDictionary = new Dictionary<string, string>();
+             
             List<Translation> translatedWords = new();
             if(File.Exists(filename))
             {
@@ -24,9 +27,23 @@ namespace DictionaryLesson
                 while(line != null)
                 {
                     //how do i use the object Word and Meaning here 
-                    
-                    
-     
+                    // Now you split the line at the ";"
+                    String [] words = line.Split(';');
+                    foreach (string item in words)
+                    {
+                        // Now, each item looks like this...
+                        // House:Maison
+                        // So, we split the item at the ':"
+                        // Declare an array called 'meaningOfWord' to hold the items we just split
+                        // Now, meaningOfWord[0] is the key and meaningOfWord[1] is the value.
+                        // so, add the <key, value> to the dictionary 'wordsInDictionary'
+                        // declare 2 variables of type string 
+                        // string key = holds the key, which is meaningOfWord[0]
+                        // string value = holds the key, which is meaningOfWord[1]
+                        // Now add the <key, value>; pair to the dictionary.
+                    }
+
+
 
                     line = file.ReadLine();
                 }
