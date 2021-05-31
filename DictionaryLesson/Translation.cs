7cc38eed50d04b82fbf18242236a588cce42a95d
Sofiane taleb <str14.don@gmail.com>
2021-05-31 15:35:05 +0200

    push the work

--- DictionaryLesson/Translation.cs
+++ DictionaryLesson/Translation.cs
@@ -33,12 +33,56 @@ namespace DictionaryLesson
                         string value = meaningOfWord[1];
 
                         Console.WriteLine("Please enter an English word: ");
-                        key = Console.ReadLine();
-                        Console.WriteLine($"The French word for '{key}' is '{value}'");
-                        Console.ReadLine();
+                        // key = Console.ReadLine();
+                        //Console.WriteLine($"The French word for '{key}' is '{value}'");
+                        while (key != value)
+                        {
+
+                            key = Console.ReadLine();
+
+                            if (!string.(key, out value))
+                            {
+                                Console.WriteLine("Please enter alphabetic characters only, with 3 characters or more");
+
+                                continue;
+                            }
+
+
+
+
+                            if (key != value)
+                            {
+                                Console.WriteLine("Sorry, this word is too short. Please enter a word with 3 characters or more");
+                                
+                            }
+                        }     
                         
+                       // Console.WriteLine($"The French word for '{key}' is '{value}'");
+
+                        Console.WriteLine("This word does not exist in the dictionary");
+                        
+                        Console.WriteLine(" Would you like to add it to the dictionary(Y or N) ? ");
+
+                        string answer = Console.ReadLine().ToUpper();
+
+                        if (answer == "Y")
+                        {
+                            Console.WriteLine("Please enter an English word, separated by ':'  ");
+                            StreamWriter sw = new StreamWriter(filename);
+                            sw.WriteLine(item);
+                            sw.Close();
+                            Console.WriteLine($"The word '{key}' has been added to the dictionary");
+                            continue;
+                        }
+                        else if (answer == "N")
+                        {
+                            Console.WriteLine($"You have chosen not to add the '{value}' to the dictionay");
+                            continue;
+                        }
+                       
                     }
                     line = file.ReadLine();
+                   
                 }
                 file.Close();
             }
