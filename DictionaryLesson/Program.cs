10e345740b7272ab3621d956ceff4e74c1db873f
mfaci2000 <mfaci2000@yahoo.com>
2021-06-04 17:31:37 -0500

    Cleaned the code.

--- DictionaryLesson/Program.cs
+++ DictionaryLesson/Program.cs
@@ -10,76 +10,27 @@ namespace DictionaryLesson
     class Program
     {
         static void Main(string[] args)
-        {
-
-            // Task 1.
-            //      1.1
-            //      If the user enters a word with 2 characters or less, the system displays
-            //      Sorry, this word is too short. Please enter a word with 3 characters or more.
-            //
-            //     1.2.
-            //     If the user enters non-alphabetic characters (like 1, 2, $, %, *, and so on), the system displays:
-            //     Please enter alphabetic characters only, with 3 characters or more.
-           
+        {           
             string userInput = StringProcessing.GetValidInput();
            
-            // Console.WriteLine($"The word '{userInput}' is acceptable");
-
-
-            // Task 2.
-            // Cpmplete this method.
             bool isWordInDictionary = Translation.VerifyWordInDictionary(userInput);
 
-            // Complete the following method.
-            // The method adds the word to dictionary if it does not exist.
-
-            // Task 3
             Translation translation = new Translation();
+
+            // Add a word to the dictionary
             translation.AddWordToDictionary(userInput, isWordInDictionary);
+
+            // Print the word we just added to the dictionay
             translation.PrintDictionary();
-            // See this page for samples to write a dictionary tpo a file.
-            //string myPath = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile);
-            //string dictionaryLessonDir = Directory.GetParent(myPath).ToString();
-            //translation.CreateFile();
-            Console.WriteLine("Original file.....");
+
+            // Print the dictionary file before adding the word to it.
             translation.PrintFileToScreen();
+
+            // Add the new word to the dictionary if it is not alreday in the dictionary.
             translation.SaveDictionaryToFile();
 
-            Console.WriteLine("Modified file.....");
+            // Print the updated dictionary file. It should contain the new word that we just added.
             translation.PrintFileToScreen();
-            // Here is  the description for this method.
-            //   3.a)
-            //   If the word that does not exist in the dictionary, the program displays a message saying:
-            //   This word does not exist in the dictionary.
-            //   (Assume the user entered 'Sky')
-            //    Would you like to add it to the dictionary (Y or N)?
-            //    
-
-            //    3.b)
-            //    If the user enters 'Y', the system enters the message:
-            //    Please enter a french word for this English word:
-            //    For example: Ciel
-            //   
-            //    Once the word is added to the dictionry, the system displays:
-            //    The word 'Sky' has been added to the dictionary
-            //    
-            //    The system continues normally with the message
-            //    Please enter an English word:
-            //
-            //    3.c)
-            //    If the user enters, 'N', then the word is not added to the dictionary.
-            //    The system displays:
-            //    You have chosen not to add the word 'Ciel' to the dictionay.
-            //    Then, the system continues normally with the message
-            //    Please enter an English word:
-            // 
-            // Let me know if you have any questions about what needs to be done.
-            // (in software, we call these requirements)
-            // 
-            // Feel free to make any changes to get the program to make it work.
-
-            //  Translation.GetDictionaryData();
-
 
         }
     }
--- DictionaryLesson/Translation.cs
+++ DictionaryLesson/Translation.cs
@@ -9,54 +9,11 @@ namespace DictionaryLesson
 {
     class Translation
     {
-        //private static string key;
-        //public Dictionary<string, string> wordsInDictionary;
         private static string fileName = @"EnglishDictionay.txt";
         public static Dictionary<string, string> wordsInDictionary = new Dictionary<string, string>();
 
         public string frenchWord;
 
-        public void CreateFile()
-        {
-            //string fileName = @"EnglishDictionayHello.txt";
-            string currentDirectory = Directory.GetCurrentDirectory();
-            string startupPath = Directory.GetParent(currentDirectory).Parent.Parent.FullName;
-            string fileRelativePath = Path.Combine(startupPath, fileName);
-
-            if (!File.Exists(fileRelativePath))
-            {
-                // Create a file to write to.
-                using (StreamWriter sw = File.CreateText(fileRelativePath))
-                {
-                    sw.WriteLine("Hello");
-                    sw.WriteLine("And");
-                    sw.WriteLine("Welcome");
-                }
-            }
-
-
-
-            //string projectPath = Path.GetFullPath(@"..\..\..");
-
-            //var relativeProjectPath = @"..\DictionaryLesson\";
-
-
-            //string filePath = Directory.GetParent(Directory.GetParent(filename).ToString()).ToString();
-            //String path = "..\\FolderIcon\\Folder.ico"
-
-            //string currentDir = Directory.GetCurrentDirectory().Parent.Parent.Parent.FullName;//.GetParent(path).ToString();
-            //string dictionaryLessonDir = Directory.GetParent(path);
-            //string fileLocation = Directory.GetParent(dictionaryLessonDir).ToString();
-            // Open the file to read from.
-            using (StreamReader sr = File.OpenText(fileRelativePath))
-            {
-                string s = "";
-                while ((s = sr.ReadLine()) != null)
-                {
-                    Console.WriteLine(s);
-                }
-            }
-        }
 
         public void PrintFileToScreen()
         {
@@ -73,101 +30,10 @@ namespace DictionaryLesson
             }
         }
 
-
-
-        //public Dictionary<string, string> GetDictionaryData()
-        //{
-        //    //    string filename = @"TranslationFile.txt";
-        //    //    //Dictionary<string, string> wordsInDictionary = new Dictionary<string, string>();
-
-        //    if (File.Exists(filename))
-        //    {
-        //        StreamReader file = new(filename);
-        //        string line = file.ReadLine();
-
-
-        //        while (line != null)
-        //        {
-
-        //            String[] words = line.Split(';');
-
-        //            foreach (string item in words)
-        //            {
-        //                String[] meaningOfWord = item.Split(':');
-        //                string key = meaningOfWord[0];
-        //                string value = meaningOfWord[1];
-
-        //                Console.WriteLine("Please enter an English word: ");
-        //                // key = Console.ReadLine();
-        //                //Console.WriteLine($"The French word for '{key}' is '{value}'");
-
-        //                //it's correct when i declare while loop?
-        //                // or i don't need to declare a while loop ?
-        //                while (key != value)
-        //                {
-
-        //                    key = Console.ReadLine();
-
-        //                    // if this code is  correct 
-        //                    // what i should write after "." ?
-        //                    //if (!string.(key, out value))
-        //                    //{
-        //                    //    Console.WriteLine("Please enter alphabetic characters only, with 3 characters or more");
-
-        //                    //    continue;
-        //                    //}
-
-
-
-
-        //                    if (key != value)
-        //                    {
-        //                        Console.WriteLine("Sorry, this word is too short. Please enter a word with 3 characters or more");
-
-        //                    }
-        //                }
-
-        //                Console.WriteLine($"The French word for '{key}' is '{value}'");
-
-        //                Console.WriteLine("This word does not exist in the dictionary");
-
-        //                Console.WriteLine(" Would you like to add it to the dictionary(Y or N) ? ");
-
-        //                string answer = Console.ReadLine().ToUpper();
-
-        //                if (answer == "Y")
-        //                {
-        //                    Console.WriteLine("Please enter an English word, separated by ':'  ");
-        //                    StreamWriter sw = new StreamWriter(filename);
-        //                    sw.WriteLine(item);
-        //                    sw.Close();
-        //                    Console.WriteLine($"The word '{key}' has been added to the dictionary");
-        //                    continue;
-        //                }
-        //                else if (answer == "N")
-        //                {
-        //                    Console.WriteLine($"You have chosen not to add the '{value}' to the dictionay");
-        //                    continue;
-        //                }
-
-        //            }
-        //            //            line = file.ReadLine();
-
-        //        }
-        //        //        file.Close();
-        //    }
-        //}   // return wordsInDictionary;
-        //    //}
-
-
-
         public static bool VerifyWordInDictionary(string userInput)
         {
             // If 'userInput' exists in the dictionary, the program return true
             // If 'userInput' does not exist in the dictionary, the program return false
-
-            // This variable 'wordsInDictionary' will always be empty, because it is declared inside the method.
-            // So, to make accessible, I have moved to the top and made it public.
 
             bool keyExists = wordsInDictionary.ContainsKey(userInput);
             if (keyExists)
@@ -198,38 +64,10 @@ namespace DictionaryLesson
                     Console.WriteLine($"The word '{userInput}' has been added to the dictionary");
                 }
                 if (answer == "N")
-                {
-                   
-                        Console.WriteLine($"You have chosen not to add the word '{userInput}' to the dictionay.");
-                   
+                { 
+                    Console.WriteLine($"You have chosen not to add the word '{userInput}' to the dictionay.");      
                 }
             }
-            // question 2:
-            //if (isWordInDictionary == true)
-            //{
-
-            //    StreamReader file = new(filename);
-            //    string line = file.ReadLine();
-            //    while (line != null)
-            //    {
-
-            //        String[] words = line.Split(';');
-
-            //        foreach (string item in words)
-            //        {
-            //            String[] meaningOfWord = item.Split(':');
-
-            //            string value = meaningOfWord[0];
-            //            Console.WriteLine($"The French word for '{userInput}' is '{value}'");
-            //        }
-            //    }
-            //    file.Close();
-            //    foreach (char t in userInput)
-            //    {
-            //       Console.WriteLine($"The French word for '{t}' is '{frenchWord}'");
-            //    }
-
-           // }
         }
         public void PrintDictionary()
         {
@@ -241,9 +79,7 @@ namespace DictionaryLesson
             }
             Console.WriteLine(output);
         }
-         //question :
-         // i wrote this code , but no result 
-         // if you can explain to me to fix it
+
         public void SaveDictionaryToFile()
         {
             string currentDirectory = Directory.GetCurrentDirectory();
@@ -254,69 +90,9 @@ namespace DictionaryLesson
             {
                 foreach (var entry in wordsInDictionary)
                 {
-                    //SaveEntryToFile(w, entry);
                     w.WriteLine("{0} {1}", entry.Key, entry.Value);
                 }
             }
-
-            
-
-            //using (StreamWriter w = File.AppendText(filename))
-            //{
-            //    foreach (var entry in wordsInDictionary)
-            //    {
-            //        w.WriteLine("{0} {1}", entry.Key, entry.Value);
-            //    }
-            //}
-
-            //using StreamWriter file = new StreamWriter(filename);
-            //   foreach (var entry in wordsInDictionary)
-            //       file.WriteLine("{0} {1}", entry.Key, entry.Value);
-            //file.Close();
-        
-        }
-
-        private void SaveEntryToFile(StreamWriter w, KeyValuePair<string, string> entry)
-        {
-            try
-            {
-                w.WriteLine("{0} {1}", entry.Key, entry.Value);
-                //w.WriteAllText("c:\\temp\\output.txt", "Hello World");
-            }
-            catch (DirectoryNotFoundException dirNotFoundException)
-            {
-                Console.WriteLine(dirNotFoundException.Message);
-            }
-            catch (UnauthorizedAccessException unauthorizedAccessException)
-            {
-                Console.WriteLine(unauthorizedAccessException.Message);
-            }
-            catch (IOException ioException)
-            {
-                Console.WriteLine(ioException.Message);
-                //logger.Error(ioException, "Error during file write");
-                // Show a message to the user
-            }
-            catch (Exception exception)
-            {
-                Console.WriteLine(exception.Message);
-                //logger.Fatal(exception, "We need to handle this better");
-                // Show general message to the user
-            }
-        }
-
-        public static void DumpLog()
-        {
-            using (StreamReader r = File.OpenText(fileName))
-            {
-              string line;
-            while ((line = r.ReadLine()) != null)
-            {
-                Console.WriteLine(line);
-            }
-            }
-
-
         }
     }
 }
